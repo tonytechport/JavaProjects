a1578114295efa82139078af6907da26002b30c6
tonytechport <tonytechport@gmail.com>
2019-10-14 13:14:56 -0500

    Finished LeapYear

---
+++ LeapYearCalculator/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String args[]){
+
+        System.out.println(LeapYearCalculator.isLeapYear(1700));
+        System.out.println(LeapYearCalculator.isLeapYear(1600));
+
+    }
+
+}
