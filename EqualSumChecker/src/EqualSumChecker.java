f6641ed557dba1bba5984e45c7f0bff72109ce47
tonytechport <tonytechport@gmail.com>
2019-10-14 21:37:03 -0500

    Finished EqualSumChecker

---
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,11 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int x, int y, int z){
+
+        int sum = x + y;
+
+        return sum == z;
+
+    }
+
+}
---
+++ EqualSumChecker/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println(EqualSumChecker.hasEqualSum(1, 1, 2));
+
+    }
+
+}
