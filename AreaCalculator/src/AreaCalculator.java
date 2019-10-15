ae97e5843b1d477b58fee8f213af530542d8e2d9
tonytechport <tonytechport@gmail.com>
2019-10-15 13:35:39 -0500

    Finished Area Calculator

---
+++ AreaCalculator/src/AreaCalculator.java
@@ -0,0 +1,24 @@
+import java.lang.Math;
+
+public class AreaCalculator {
+
+    public static double area(double radius){
+        if(radius < 0){
+            return -1;
+        }
+        else{
+            return radius * radius * Math.PI;
+        }
+    }
+
+    public static double area(double x, double y){
+        if(x < 0 || y < 0){
+            return -1;
+        }
+        else {
+            return x * y;
+        }
+
+    }
+
+}
